5abd607ba43f6bb336263471d04c51d6b68506b2
qwqcode <1149527164@qq.com>
2020-04-27 14:50:41 +0800

    Add function "Read More"

--- app/components/Action.php
+++ app/components/Action.php
@@ -14,7 +14,7 @@ trait Action
     if (empty($nick)) return $this->error('昵称 不能为空');
     if (empty($email)) return $this->error('邮箱 不能为空');
     if (empty($password)) return $this->error('密码 不能为空');
-    
+
     if (!$this->isAdmin($nick, $email)) {
       return $this->error('无需管理员权限');
     }
@@ -24,17 +24,17 @@ trait Action
       return $this->error('密码错误');
     }
   }
-  
+
   public function actionCaptchaCheck()
   {
     if (!empty(trim($_POST['refresh'] ?? ''))) {
       $imgData = $this->refreshGetCaptcha();
       return $this->success('验证码刷新成功', ['img_data' => $imgData]);
     }
-    
+
     $captcha = trim($_POST['captcha'] ?? '');
     if (empty($captcha)) return $this->error('验证码 不能为空');
-    
+
     if ($this->checkCaptcha($captcha)) {
       return $this->success('验证码正确');
     } else {
@@ -42,7 +42,7 @@ trait Action
       return $this->error('验证码错误', ['img_data' => $imgData]);
     }
   }
-  
+
   public function actionCommentAdd()
   {
     $content = trim($_POST['content'] ?? '');
@@ -54,7 +54,7 @@ trait Action
     $ua = $_SERVER['HTTP_USER_AGENT'] ?? '';
     $password = trim($_POST['password'] ?? '');
     $captcha = trim($_POST['captcha'] ?? '');
-    
+
     if ($this->isAdmin($nick, $email) && !$this->checkAdminPassword($nick, $email, $password)) {
       return $this->error('需要管理员身份', ['need_password' => true]);
     }
@@ -62,14 +62,14 @@ trait Action
       $imgData = $this->refreshGetCaptcha(); // 生成新的验证码
       return $this->error('需要验证码', ['need_captcha' => true, 'img_data' => $imgData]);
     }
-    
+
     if (empty($pageKey)) return $this->error('pageKey 不能为空');
     if (empty($nick)) return $this->error('昵称不能为空');
     if (empty($email)) return $this->error('邮箱不能为空');
     if (empty($content)) return $this->error('内容不能为空');
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return $this->error('邮箱格式错误');
     if (!empty($link) && !Utils::urlValidator($link)) return $this->error('网址格式错误');
-    
+
     $commentData = [
       'content' => $content,
       'nick' => $nick,
@@ -84,40 +84,72 @@ trait Action
     $comment = self::getCommentsTable();
     $comment->set($commentData);
     $comment->save();
-  
+
     $commentData['id'] = $comment->lastId();
-    
+
     $this->refreshGetCaptcha(); // 刷新验证码
     try {
       Utils::sendEmailToCommenter($commentData); // 发送邮件通知
     } catch (\Exception $e) {
       return $this->error('通知邮件发送失败，请联系网站管理员', ['error-msg' => $e->getMessage(), 'error-detail' => $e->getTraceAsString()]);
     }
-    
+
     return $this->success('评论成功', ['comment' => $this->beautifyCommentData($commentData)]);
   }
-  
+
   public function actionCommentGet()
   {
     $pageKey = trim($_POST['page_key'] ?? '');
     if (empty($pageKey)) {
       return $this->error('page_key 不能为空');
     }
-    
-    $commentsRaw = self::getCommentsTable()
+
+    $offset = intval(trim($_POST['offset'] ?? 0));
+    $limit = intval(trim($_POST['limit'] ?? 0));
+    if ($offset < 0) $offset = 0;
+    if ($limit <= 0) $limit = 30;
+
+    $commentTable = self::getCommentsTable();
+
+    $comments = [];
+    $QueryAllChildren = function ($parentId) use (&$commentTable, &$pageKey, &$comments, &$QueryAllChildren) {
+      $rawComments = $commentTable
+        ->where('page_key', '=', $pageKey)
+        ->where('rid', '=', $parentId)
+        ->orderBy('date', 'DESC')
+        ->findAll()
+        ->asArray();
+
+      foreach ($rawComments as $item) {
+        $comments[] = $this->beautifyCommentData($item);
+        $QueryAllChildren($item['id']);
+      }
+    };
+
+    $commentsRaw = $commentTable
       ->where('page_key', '=', $pageKey)
+      ->where('rid', '=', 0)
       ->orderBy('date', 'DESC')
+      ->limit($limit, $offset)
       ->findAll()
       ->asArray();
-    
-    $comments = [];
+
     foreach ($commentsRaw as $item) {
       $comments[] = $this->beautifyCommentData($item);
+
+      // Child Comments
+      $QueryAllChildren($item['id']);
     }
-    
-    return $this->success('获取成功', ['comments' => $comments]);
+
+    return $this->success('获取成功', [
+      'comments' => $comments,
+      'offset' => $offset,
+      'limit' => $limit,
+      'total_parents' => $commentTable->where('page_key', '=', $pageKey)->where('rid', '=', 0)->findAll()->count(),
+      'total' => $commentTable->where('page_key', '=', $pageKey)->findAll()->count()
+    ]);
   }
-  
+
   private function beautifyCommentData($rawComment)
   {
     $comment = [];
@@ -127,7 +159,7 @@ trait Action
         $comment[$key] = $value;
       }
     }
-    
+
     $comment['email_encrypted'] = md5(strtolower(trim($rawComment['email'])));
     $comment['badge'] = null;
     if ($this->isAdmin($rawComment['nick'] ?? null, $rawComment['email'] ?? null)) {
@@ -135,16 +167,16 @@ trait Action
     }
     return $comment;
   }
-  
+
   public function actionCommentReplyGet()
   {
     $nick = trim($_POST['nick'] ?? '');
     $email = trim($_POST['email'] ?? '');
     if (empty($nick)) return $this->error('昵称 不能为空');
     if (empty($email)) return $this->error('邮箱 不能为空');
-    
+
     $replyRaw = self::getCommentsTable();
-    
+
     if (!$this->isAdmin($nick, $email)) {
       $myComments = self::getCommentsTable()
         ->where('nick', '=', $nick)
@@ -152,28 +184,28 @@ trait Action
         ->orderBy('date', 'DESC')
         ->findAll()
         ->asArray();
-  
+
       $idList = [];
       foreach ($myComments as $item) {
         $idList[] = $item['id'];
       }
-      
+
       $replyRaw = $replyRaw->where('rid', 'IN', $idList);
     }
-    
+
     $replyRaw = $replyRaw
       ->orderBy('date', 'DESC')
       ->findAll()
       ->asArray();
-  
+
     $reply = [];
     foreach ($replyRaw as $item) {
       $reply[] = $this->beautifyCommentData($item);
     }
-    
+
     return $this->success('获取成功', ['reply_comments' => $reply]);
   }
-  
+
   /*public function actionTest()
   {
     // 测试过后记得清理
